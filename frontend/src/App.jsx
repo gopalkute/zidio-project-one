07e008529e236ba6e61972510b2087d68fdd9611
PratikMeghnathi <140478668+PratikMeghnathi@users.noreply.github.com>
2025-05-01 16:53:11 +0530

    conflict changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from "react-router"
 import Router from "./routes"
-import { Toaster } from "react-hot-toast"
 
 function App() {
   return (
       <>
       <RouterProvider router={Router} />
-      <Toaster />
       </>
   )
 }
